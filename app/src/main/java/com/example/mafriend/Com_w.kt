e006e9164c19d417853d652525e455e9c4885380
sia7997 <sia7997@naver.com>
2022-11-25 01:57:32 +0900

    build gradle 수정

--- app/src/main/java/com/example/mafriend/Com_w.kt
+++ app/src/main/java/com/example/mafriend/Com_w.kt
@@ -2,7 +2,7 @@ package com.example.mafriend
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.com_w.*
 
 class Com_w: AppCompatActivity() {
